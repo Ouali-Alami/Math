56b546b4cf59d463f3c37df6319b4ebdaad4614d
wall_e <mohouali.748@gmail.com>
2024-07-23 06:54:32 +0200

    Update divisibility_criteria.py

--- divisibility_criteria.py
+++ divisibility_criteria.py
@@ -17,12 +17,25 @@ def IsDivisbleByThree(x) -> bool:
     else:
         return sum % 3 == 0
 
-# A ≡ B [N] <=> B = A + N*K <=> B-A = N*K put another way N divide( B - A) K times
+# A ≡ B [N] <=> B = A - N*K <=> B-A = -N*K put another way N divide( B - A) K times
+# by symetrie A ≡ B [N] <=> A = B + N*K <=> A -B = N*K with K in {Z}
 # another way to tell A//N and B//N have the same remainder in Euclidean division
 # cool there is already a method that tell if a number is divisble by 3...
 # source : https://www.youtube.com/watch?v=BByKm1u16qs (congruence)    
 def IsCongruentModThree(a , b) -> bool:
+    # here b - a or a - b doesn't matter example -6 mod 3 = 0 and 6 mod 3 = 0
+    if a == b: return True
     return IsDivisbleByThree(b - a)
+
+# To resume with congruence u can do like everything with the modulo
+# u can simplify r and (sub r) or l (sub l) values by the modulo
+# there are more than 4 ways to solve congruent equation
+# the major law of composition AND properties of {N}  are applicable
+# 4 things to remember:
+#1 - k can be negative (b>a)
+#2 - a = b + nk
+#3 - u can turn neg to pos number with -+mod n try to simply at min(-1 , 1)
+#4 - very useful for big numbers like exp... everything is about distance...
 
 
 # todo : https://www.youtube.com/watch?v=c51T2kZpkS4 (divisble by 7)
